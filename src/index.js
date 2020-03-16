5c74e47bc625eed6812cfe03a69da414b59e3683
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-16 07:14:46 +0545

    bootstrap css package added to project

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
+
 import RandomAlphabet from './components/random-alphabet';
 import * as serviceWorker from './serviceWorker';
 

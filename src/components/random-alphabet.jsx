c72479cdef562ac2d1a0b9170d78878d0ad2b277
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-16 07:07:14 +0545

    random letters of 4x4 are rendered on dom from random-alphabet component

--- src/components/random-alphabet.jsx
+++ src/components/random-alphabet.jsx
@@ -1,51 +1,69 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class RandomAlphabet extends Component {
-    state = { 
-        characters: "EARIONTNSLC",
-        charArray:[]
-     }
-    render() { 
-        return ( 
-            <h2>Random alphabet component</h2>
-         );
+  state = {
+    characters: "EARIONTNSLC",
+    charArray: []
+  };
+  render() {
+    return this.state.charArray.map(x => (
+        <table>
+            <thead>
+            </thead>
+            <tbody>
+                {x.map((y,i)=>(
+                    <tr>
+                        {y.map((z,j)=>(
+                            <td>
+                                <button className="btn btn-primary btn-sm"
+                                data-row-id={i}
+                                data-col-id={j}>
+                                    {z}
+                                </button>
+                            </td>
+                        ))}
+                    </tr>
+                ))}
+            </tbody>
+        </table>
+    )
+        );
+  }
+
+  componentDidMount() {
+    this.getRandomAlphabets();
+  }
+
+  getRandomAlphabets() {
+    const { characters } = this.state;
+
+    var wordArray = new Array(4);
+    for (var i = 0; i < 4; i++) {
+      wordArray[i] = new Array(4);
     }
 
-    componentDidMount(){
-        this.getRandomAlphabets();
-        console.log(this.state.charArray);
+    var word = "";
+
+    for (var i = 0; i <= 15; i++) {
+      var charPos = Math.floor(Math.random() * characters.length);
+      word += characters.charAt(charPos);
     }
 
-    getRandomAlphabets(){
-        const {characters} = this.state;
+    var l = 0;
 
-        var wordArray = new Array(4);
-        for(var i=0; i<4; i++){
-            wordArray[i]=new Array(4);
-        }
-
-        var word = "";
-
-        for(var i=0; i<=15; i++){
-            var charPos = Math.floor(Math.random() * characters.length);
-            word += characters.charAt(charPos);
-        }
-
-        var l = 0;
-
-        for(var j=0; j<4; j++){
-            for(var k=0; k<4; k++){
-                wordArray[j][k] = word[l];
-                l++;
-            }
-        }
-
-        this.setState(state=>{
-            const charArray = state.charArray.push(wordArray);
-
-            return charArray;
-        });
+    for (var j = 0; j < 4; j++) {
+      for (var k = 0; k < 4; k++) {
+        wordArray[j][k] = word[l];
+        l++;
+      }
     }
+
+    this.setState(state => {
+      const charArray = state.charArray.push(wordArray);
+
+      return charArray;
+    });
+  }
 }
- 
+
 export default RandomAlphabet;

fcbd45b5fd3ec8a6eff0161fef1eb2d89261c6a9
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-16 19:23:01 +0545

    second and minute states added to timer component and rendered it

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
 
 class Timer extends Component {
-    state = {  }
+    state = { 
+        second: 59,
+        minute: 2
+     }
     render() { 
+        let displayMinute = this.state.minute-1;
+        let timeText = this.state.second>=10? displayMinute+":"+this.state.second : displayMinute+':0'+this.state.second;
         return ( 
-            <h2>This is timer component</h2>
+            <h3>
+                Time Left{" "}
+                <span className="badge badge-success">
+                    {timeText}
+                </span>
+            </h3>
          );
     }
 }

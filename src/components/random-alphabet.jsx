f1ddcb95d16e8dc616bad085fbc4dd0147163bb3
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-16 06:52:28 +0545

    random-alphabet component added

---
+++ src/components/random-alphabet.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class RandomAlphabet extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h2>Random alphabet component</h2>
+         );
+    }
+}
+ 
+export default RandomAlphabet;

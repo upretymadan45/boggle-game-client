681794651516f8ab33d72e6c5f103a43198921fb
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-16 07:39:30 +0545

    BoggleGame component was added

---
+++ src/components/boggle-game.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class BoggleGame extends Component {
+    state = {  }
+    render() { 
+        return ( 
+            <h2>Boggle game component</h2>
+         );
+    }
+}
+ 
+export default BoggleGame;

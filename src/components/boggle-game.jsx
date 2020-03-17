69342a5c39dd38501d0417cea94903b3bd24ec4d
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-17 19:09:59 +0545

    Auto navigation to ScoreComponent was added when game time expires

--- src/components/boggle-game.jsx
+++ src/components/boggle-game.jsx
@@ -1,43 +1,43 @@
-import React, { Component } from 'react';
-import RandomAlphabet from './random-alphabet';
-import UserInput from './user-input';
-import Timer from './timer';
+import React, { Component } from "react";
+import { withRouter } from "react-router-dom";
+
+import RandomAlphabet from "./random-alphabet";
+import UserInput from "./user-input";
+import Timer from "./timer";
 
 class BoggleGame extends Component {
+  constructor(props) {
+    super(props);
+    this.timeUp = this.timeUp.bind(this);
+  }
 
-    constructor(props){
-        super(props);
-        this.timeUp = this.timeUp.bind(this);
-    }
+  state = {
+    gameOver: false
+  };
+  render() {
+    return (
+      <div className="row" style={{ marginTop: 50 }}>
+        <div className="col-6 offset-sm-4">
+          <RandomAlphabet />
 
-    state = { 
-        gameOver: false
-    }
-    render() { 
-        return ( 
-        <div className="row" style={{marginTop: 50}}>
-            <div className="col-6 offset-sm-4">
-                <RandomAlphabet/>
-
-                <UserInput onKeyUp={this.handleUserInputOnKeyUp}/>
-
-            </div>
-            <div className="col-2">
-                <Timer onTimeUp={this.timeUp}/>
-            </div>
+          <UserInput onKeyUp={this.handleUserInputOnKeyUp} />
         </div>
-         );
-    }
+        <div className="col-2">
+          <Timer onTimeUp={this.timeUp} />
+        </div>
+      </div>
+    );
+  }
 
-    handleUserInputOnKeyUp=(event)=>{
-        let inputValue = event.target.value.toUpperCase();
-        console.log(inputValue);
-    }
+  handleUserInputOnKeyUp = event => {
+    let inputValue = event.target.value.toUpperCase();
+    console.log(inputValue);
+  };
 
-    timeUp(){
-        this.setState({gameOver: true});
-        console.log(this.state.gameOver);
-    }
+  timeUp() {
+    this.setState({ gameOver: true });
+    this.props.history.push("/score");
+  }
 }
- 
-export default BoggleGame;
+
+export default withRouter(BoggleGame);

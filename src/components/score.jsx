9f79302e7e6034e7cf67374306fc0bb87d98392b
Madan Uprety <madanuprety@Madans-MacBook-Pro.local>
2020-03-17 19:05:01 +0545

    navigation from Score to BoggleGame component was added on restart button click

--- src/components/score.jsx
+++ src/components/score.jsx
@@ -1,18 +1,27 @@
 import React, { Component } from 'react';
+import {withRouter} from 'react-router-dom';
 
 class Score extends Component {
+    constructor(props){
+        super(props);
+        this.restartGame = this.restartGame.bind(this);
+    }
     state = {  }
     render() { 
         return ( 
             <div>
                 <h2>Your score is : 50</h2>
 
-                <button className="btn btn-primary btn-sm">
+                <button className="btn btn-primary btn-sm" onClick={this.restartGame}>
                     Play Again
                 </button>
             </div>
          );
     }
+
+    restartGame(){
+        this.props.history.push("/");
+    }
 }
  
-export default Score;
+export default withRouter(Score);
